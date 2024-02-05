33756763b66b584fd2f5a9d10aa04a09def7b0a7
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-02-05 13:20:30 -0600

    Initial Commit

---
+++ carOptionsJonahWUITests/carOptionsJonahWUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  carOptionsJonahWUITestsLaunchTests.swift
+//  carOptionsJonahWUITests
+//
+//  Created by Jonah Whitney on 2/5/24.
+//
+
+import XCTest
+
+final class carOptionsJonahWUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
